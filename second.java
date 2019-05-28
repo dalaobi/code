378b014c5cbaa79811a8bfab8a63762eeb0a31fe
zhexuan <shanzhexuan@momenta.ai>
2019-05-28 20:09:10 +0800

    add new method  Please enter the commit message for your changes. Lines starting

--- second.java
+++ second.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.PriorityQueue;
 
 /**排序
@@ -74,11 +77,96 @@ class second{
         a[i] = a[j];
         a[j] = temp; 
     }
-/**
+/**to review
  * 347.top k frequent elements
- * 
+ * g
  */
     public List<Integer> topKFrequent(int[] nums, int k){
+        //step1: hashmap ,统计出不同的频率
+        HashMap <> hm = new HashMap<>();
+        for(int num : nums){
+           hm.put(num, hm.getOrDefault(num, 0)+1) ;
+        }
+        //step2: bucket[[],[] ]， 根据频率桶排序（下标即是频率）
+        // hm.keyset() , hm.get(key)
+        List<Integer> [] buckets = new ArrayList [nums.length+1];
+        for(int key : hm.keyset()){
+            int frequency = hm.get(key);
+            if(buckets[frequency] == null) buckets[frequency] = new ArrayList();
+            else buckets[frequency].add(key);
+        }
+        //step3: find top k
+        //todo:  List<Integer> [] buckets = new ArrayList [nums.length+1];
+        //todo: list.size() ???? if list[i] = null
+        // addAll, 传入是一个list，将list所有元素加入当前 list中 
+        //sublist(int fromindex, int endindex), 返回一个子数组， 原数组和子数组的操作互相影响
+        List<Integer>  topK = new ArrayList<>();
+        for(i = buckets.length; i>=0 && topK.size() <k; i--){
+            //! 防止 bucketes[i]长度不够报错, buckets[i]长度超过k 溢出
+            if(buckets[i].size() == 0){
+                continue;
+            }
+            else if(buckets[i].size() < (k- topK.size())){
+                topK.addAll(buckets[i]);
+            }
+            else{
+                topK.addAll(buckets[i].subList(0, k-topK.size()));
+            }
+
+        }
+        return topK;
+    }
+/**Sort Characters By Frequency 
+ * i: string.toCharArray()
+ * 
+ */
+    public String frequencySort(String s) {
+        HashMap <> numFrequency = new HashMap<>();
+        for(char i : s.toCharArray()){
+            numFrequency.put(i, numFrequency.getOrDefault(i, 0)+1);
+        }
+        List<>[] frequencyBucket = new ArrayList[s.length()+1];
+        for(char key: numFrequency.keySet()) {
+            int frequency = frequencyBucket.get(key);
+            if(frequencyBucket[frequency] == null){
+                frequencyBucket[frequency] = new ArrayList<>();
+            }
+            frequencyBucket[frequency].add(key);
+        } 
+        StringBuilder str = new StringBuilder();
+        for(int i = frequencyBucket.length; i>=0 ;i--){
+            if(frequencyBucket[i] == 0) continue;
+            for(char c: frequencyBucket[i]){
+                //!  for (int j = 0; j < i; j++)  j从0开始到i（频率），来保证 字符出现几次就append几次
+                for(int j =0; j<i; j++){
+                str.append(c);
+            }
+            }
+        }
+        return str.toString();
+    }
+    /** 75. Sort Colors (Medium)
+     * 0/1/2 归并排序， 思路类似快排
+     * !! swap(nums, ++zero, one++)   one++ 画图分析，若(nums, ++zero, one)，则是zero在移动， 直到swap 过来1， 否则one 不动。
+     * !应该为zero，和one 相邻， 同步移动。  设计实例， 画图分析
+     * 
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        int zero = -1,one = 0, two = nums.length;
+        // one 作为当前遍历指针。
+        while(one < two){
+            if(nums[one] == 0){swap(nums, ++zero, one++);}
+            else if(nums[one] == 2){swap(nums, --two, one);}
+            else one++;
+        }
+    } 
+
+
+
+    
+    public static void main(String[] args) {
+        List<>[] frequencyBucket = new ArrayList[10];
         
     }
 }
